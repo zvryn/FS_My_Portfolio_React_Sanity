4d59323e1eb6ea1f9cb54911cfaf9b4f10bce3d2
zvryn <sarinkanok@gmail.com>
2024-11-07 16:27:06 +0700

    feat:add navbar component

--- client/sarin-portfolio/src/components/Navbar/Navbar.jsx
+++ client/sarin-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,42 @@
 import "./Navbar.scss";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <nav>
-      <div></div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <h1>Jay Sarin</h1>
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li key={`link-${item}`} className="app__flex p-text">
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {["home", "about", "work", "skills", "contact"].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`}>{item}</a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
