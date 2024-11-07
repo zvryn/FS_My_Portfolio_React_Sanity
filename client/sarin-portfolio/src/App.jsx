003231da835d626a2cdd37f8217062111b0b009c
zvryn <sarinkanok@gmail.com>
2024-11-07 12:57:45 +0700

    feat:add folder structure container

--- client/sarin-portfolio/src/App.jsx
+++ client/sarin-portfolio/src/App.jsx
@@ -1,5 +1,16 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/sarin-portfolio/src/container/Work/Work.jsx
@@ -0,0 +1,6 @@
+import "./Work.scss";
+const Work = () => {
+  return <div>Works</div>;
+};
+
+export default Work;
