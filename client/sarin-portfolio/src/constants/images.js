e302a156b0a64b6e151015aec4ab29b0cf0952c8
zvryn <sarinkanok@gmail.com>
2024-11-07 14:38:25 +0700

    feat:add constant images.js and index.js

---
+++ client/sarin-portfolio/src/constants/images.js
@@ -0,0 +1,45 @@
+import canva from "../assets/canva.webp";
+import css from "../assets/css.png";
+import express from "../assets/express.jpg";
+import figma from "../assets/figma.png";
+import git from "../assets/git.png";
+import drilling from "../assets/drilling.jpg";
+import exploration from "../assets/exploration.jpg";
+import headshotblack from "../assets/headshotblack.jpeg";
+import headshotwhite from "../assets/headshotwhite.jpeg";
+import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import mongodb from "../assets/mongodb.png";
+import nodejs from "../assets/nodejs.png";
+import olympiad from "../assets/olympiad.png";
+import postgresql from "../assets/postgresql.png";
+import rbaudit from "../assets/rbaudit.jpg";
+import rbbottle from "../assets/rbbottle.jpg";
+import react from "../assets/react.png";
+import scss from "../assets/scss.png";
+import student from "../assets/student.png";
+import tailwind from "../assets/tailwind.png";
+
+export default {
+  canva,
+  css,
+  express,
+  figma,
+  git,
+  drilling,
+  exploration,
+  headshotblack,
+  headshotwhite,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  olympiad,
+  postgresql,
+  rbaudit,
+  rbbottle,
+  react,
+  scss,
+  student,
+  tailwind,
+};
