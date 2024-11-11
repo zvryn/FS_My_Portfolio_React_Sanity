820887c48b428567e135bb05767bbf1b9d13282f
zvryn <sarinkanok@gmail.com>
2024-11-11 15:43:57 +0700

    feat:adjust constant images.js

--- client/sarin-portfolio/src/constants/images.js
+++ client/sarin-portfolio/src/constants/images.js
@@ -3,21 +3,13 @@ import css from "../assets/css.png";
 import express from "../assets/express.png";
 import figma from "../assets/figma.png";
 import git from "../assets/git.png";
-import drilling from "../assets/drilling.jpg";
-import exploration from "../assets/exploration.jpg";
-import headshotblack from "../assets/headshotblack.jpeg";
-import headshotwhite from "../assets/headshotwhite.jpeg";
 import html from "../assets/html.png";
 import javascript from "../assets/javascript.png";
 import mongodb from "../assets/mongodb.webp";
 import nodejs from "../assets/nodejs.png";
-import olympiad from "../assets/olympiad.png";
 import postgresql from "../assets/postgresql.png";
-import rbaudit from "../assets/rbaudit.jpg";
-import rbbottle from "../assets/rbbottle.jpg";
 import react from "../assets/react.png";
 import scss from "../assets/scss.png";
-import student from "../assets/student.png";
 import tailwind from "../assets/tailwind.png";
 import mern from "../assets/mern.png";
 import hero from "../assets/hero.png";
@@ -35,21 +27,13 @@ export default {
   express,
   figma,
   git,
-  drilling,
-  exploration,
-  headshotblack,
-  headshotwhite,
   html,
   javascript,
   mongodb,
   nodejs,
-  olympiad,
   postgresql,
-  rbaudit,
-  rbbottle,
   react,
   scss,
-  student,
   tailwind,
   mern,
   hero,
